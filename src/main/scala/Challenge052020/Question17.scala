a8f9c4b0cc616d037fced49dbfdf5bb3de082a5d
Hang JI <merlinwing@gmail.com>
2020-05-17 23:44:22 +0200

    add solution2 for Question 17

--- src/main/scala/Challenge052020/Question17.scala
+++ src/main/scala/Challenge052020/Question17.scala
@@ -45,8 +45,27 @@ object Solution1 {
   def isAnagram(textA: String, sortedTextB: String): Boolean = textA.length == sortedTextB.length && textA.sorted == sortedTextB
 }
 
+// solution with O(n + m) complexity
 object Solution2 {
+  import scala.collection.mutable._
   def findAnagrams(s: String, p: String): List[Int] = {
-    val charMapP = p.grou
+    val diffMap = HashMap.empty[Char, Int]
+    // initiate diffMap
+    p.foreach(updateMap(diffMap, _, 1))
+
+    // -1 if char exist in diffMap, remove key if value == 0
+    // +1 if char exit the subString's left bound , remove key if value == 0
+    (0 until s.length).toList.filter(index => {
+      if (index >= p.length) updateMap(diffMap, s(index - p.length), 1)
+      updateMap(diffMap, s(index), -1)
+      diffMap.isEmpty
+    }).map(_ - p.length + 1)
   }
+
+  def updateMap(map: HashMap[Char, Int], char: Char, incrementer: Int): Unit =
+    map.get(char) match {
+      case Some(number) if number + incrementer == 0 => map -= char
+      case Some(number) => map += (char -> (number + incrementer))
+      case None => map += (char -> incrementer)
+    }
 }
